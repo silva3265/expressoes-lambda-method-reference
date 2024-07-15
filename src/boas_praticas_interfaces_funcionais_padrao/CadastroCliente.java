7a8223de21acfe7639e012d1f9677d5da07349ba
silva3265 <silva3265@gmail.com>
2024-07-15 15:22:11 -0300

    Boas práticas: prefira interfaces funcionais padrão

---
+++ src/boas_praticas_interfaces_funcionais_padrao/CadastroCliente.java
@@ -0,0 +1,31 @@
+package boas_praticas_interfaces_funcionais_padrao;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Predicate;
+
+public class CadastroCliente {
+
+    private final List<Cliente> clientes = new ArrayList<>();
+
+    public List<Cliente> getClientes() {
+        return clientes;
+    }
+
+    public void adicionar(Cliente cliente) {
+        clientes.add(cliente);
+    }
+
+    public List<Cliente> consultar(Predicate<Cliente> filtro) {
+        List<Cliente> clientesFiltrados = new ArrayList<>();
+
+        for (Cliente cliente : clientes) {
+            if (filtro.test(cliente)) {
+                clientesFiltrados.add(cliente);
+            }
+        }
+
+        return clientesFiltrados;
+    }
+
+}
